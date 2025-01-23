22fe77f79b7746141049f8eccf80dee8fdc46af3
Estela Trujillo <estelatrdz.ds@outlook.com>
2025-01-22 23:05:24 -0600

    Delete 4 Funciones Adicionales directory

--- 4 Funciones Adicionales/4 Total_Prod_XCategoria.sql
+++
@@ -1,8 +0,0 @@
-#Total_Prod_XCategoria
-##Este scrip totaliza los productos por la categoria definida en la regla de negocio
-#Se utilio funcion CASE WHEN para las categorias
-
-SELECT	COUNT(CASE WHEN ListPrice <= 50 THEN "Barat" ELSE NULL END) AS "Num_Prod_BARATO",
-		COUNT(CASE WHEN ListPrice > 50 AND ListPrice<= 500 THEN "Me9di" ELSE NULL END) AS "Num_Prod_MEDIO",
-        COUNT(CASE WHEN ListPrice >500 THEN "Car" ELSE NULL END) AS "Num_Prod_CARO" 
-FROM	product
